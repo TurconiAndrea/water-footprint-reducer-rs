c09ccbab6b461ebcf570598b3aabb0dde315fe0d
TurcoEFelice <darthsuper@gmail.com>
2021-10-11 23:35:32 +0200

    Added more documentation

--- water_footprint_utils.py
+++ water_footprint_utils.py
@@ -4,6 +4,13 @@ from configuration import load_configuration
 
 
 class WaterFootprintUtils:
+    """
+    Class that represent utilities for the water footprint
+    reduction. This class provides a method for computing
+    the user score based on his reviews and orders.
+    It also provides a method for reducing the given
+    recommendations for the user.
+    """
     def __init__(self):
         config = load_configuration()
         self.orders = pd.read_pickle(config["path_orders"])
@@ -11,16 +18,41 @@ class WaterFootprintUtils:
         self.classes = ["A", "B", "C", "D", "E"]
 
     def __get_recipe_class_to_recommend(self, user_score):
+        """
+        Get the recipe categories to recommend based on the user score.
+        The category are always the two before the user score and
+        recipes with category equals to A.
+
+        :param user_score: the score of the user.
+        :return: a list containing the categories of the recipe to
+            recommend.
+        """
         usr_cls = self.classes.index(user_score)
         usr_cls_1 = usr_cls - 1 if usr_cls - 1 >= 0 else 0
         usr_cls_2 = usr_cls - 2 if usr_cls - 1 >= 1 else usr_cls
         return sorted({"A", self.classes[usr_cls_1], self.classes[usr_cls_2]})
 
-    def __get_recipe_class(self, id):
-        category = self.recipes.query(f"id == {id}")["category"].tolist()
+    def __get_recipe_class(self, recipe_id):
+        """
+        Get the category of the recipe from its id.
+
+        :param recipe_id: the id of the recipe.
+        :return: the category of the recipe if exists.
+        """
+        category = self.recipes.query(f"id == {recipe_id}")["category"].tolist()
         return category[0] if category else None
 
     def __get_user_score(self, user_id, weight=1.3):
+        """
+        Get the score of the user based on his reviews.
+        User orders are summed and weighted based on their
+        categories. Then based on the result the user score
+        is found.
+
+        :param user_id: the id of the user.
+        :param weight: the weight for the recipe categories.
+        :return: the user score.
+        """
         user_df = self.orders.query(f"user_id == {user_id}").copy()
         user_df["category"] = user_df["id"].apply(lambda x: self.__get_recipe_class(x))
         orders = user_df.groupby(by="category").count()["id"].to_dict()
@@ -44,11 +76,30 @@ class WaterFootprintUtils:
             user_cls = "E"
         return user_cls
 
-    def __get_recipe_from_id(self, recipe_id):
+    def __get_recipe_category(self, recipe_id):
+        """
+        Return the category of the recipe row from the
+        dataframe based on the recipe id.
+
+        :param recipe_id: the id of the recipe.
+        :return: the category of the recipe at the provided id.
+        """
         recipe = self.recipes.query(f"id == {recipe_id}")["category"].tolist()
         return recipe[0] if recipe else "F"
 
     def get_recommendations_correct(self, recommendations, user_id, algo_type):
+        """
+        Get the correct recipe recommendations from a list of
+        recommendations ids based on the user score and the
+        type of the algorithm.
+
+        :param recommendations: a list containing all the recommended recipes.
+        :param user_id: the id of the user.
+        :param algo_type: the type of the algorithm
+            (Content Based or Collaborative Filtering)
+        :return: a list containing all the recipes filtered by
+            water footprint.
+        """
         user_score = self.__get_user_score(user_id)
         class_to_rec = self.__get_recipe_class_to_recommend(user_score)
         return [
@@ -59,7 +110,7 @@ class WaterFootprintUtils:
             recipe_id
             for recipe_id in recommendations
             #if self.recipes.query(f"id == {recipe_id}")["category"].tolist()[0] in class_to_rec
-            if self.__get_recipe_from_id(recipe_id) in class_to_rec
+            if self.__get_recipe_category(recipe_id) in class_to_rec
         ]
 
 
